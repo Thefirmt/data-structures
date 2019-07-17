5f657011579d838958ab74539b1313527e898b7b
codegirl77 <rachelklinkatsis@gmail.com>
2019-07-17 15:25:57 -0700

    finished functional queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,26 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    console.log(storage)
+    var objArr = Object.entries(storage);
+    for (var i = 0; i < objArr.length; i++){
+      storage[i + 1] = objArr[i][1]
+    }
+    var num = objArr.length + 1;
+    storage[num] = value;
   };
 
   someInstance.dequeue = function() {
+    var objArr = Object.entries(storage);
+    if (objArr.length !== 0){
+      delete storage[objArr[0][0]]
+      return objArr[0][1]
+    }
   };
 
   someInstance.size = function() {
+    var objArr = Object.entries(storage);
+    return objArr.length;
   };
 
   return someInstance;
