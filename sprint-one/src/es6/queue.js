6e92877ade1b118cbda8ab95fd1664ef3f0ae303
codegirl77 <rachelklinkatsis@gmail.com>
2019-07-17 20:35:20 -0700

    finished assignment

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -8,7 +8,7 @@ class Queue {
   enqueue(val){
 	 var objArr = Object.entries(this.storage);
 	 	for (var i = 0; i < objArr.length; i++){
-	 		this.storage[i + 1] = objArr[i][1]
+	 		this.storage[i + 1] = objArr[i][1];
 	 	}
 	 var num = objArr.length + 1;
 	 this.storage[num] = val;
@@ -17,8 +17,8 @@ class Queue {
   dequeue(){
     var objArr = Object.entries(this.storage);
     if (objArr.length !== 0){
-      delete this.storage[objArr[0][0]]
-      return objArr[0][1]
+      delete this.storage[objArr[0][0]];
+      return objArr[0][1];
     }
   }
   size(){
